3ea87046af677c56744907584f69cd7243b7b47f
SAM4you <ander.stlouis@gmail.com>
2022-08-03 09:41:06 -0400

    Timeline Activity and Row Layout created

--- app/src/main/java/com/ander/apps/client/LoginActivity.java
+++ app/src/main/java/com/ander/apps/client/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.ander.apps.client;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -47,8 +48,8 @@ public class LoginActivity extends OAuthLoginActionBarActivity<TwitterClient> {
 	@Override
 	public void onLoginSuccess() {
 		Log.i("lonelyness", "login success");
-		// Intent i = new Intent(this, PhotosActivity.class);
-		// startActivity(i);
+		 Intent i = new Intent(this, TimelineActivity.class);
+		startActivity(i);
 	}
 
 	// OAuth authentication flow failed, handle the error
