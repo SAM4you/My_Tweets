4c905f6a34479edad92cb5dfb9f7ec06636865e6
SAM4you <ander.stlouis@gmail.com>
2022-08-08 13:54:12 -0400

    Floating button added

--- app/src/main/java/com/ander/apps/client/TimelineActivity.java
+++ app/src/main/java/com/ander/apps/client/TimelineActivity.java
@@ -87,12 +87,7 @@ public class TimelineActivity extends AppCompatActivity {
 
          populateHomeTimeline();
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+
 
     private void loadMoreData() {
         // 1. Send an API request to retrieve appropriate paginated data
